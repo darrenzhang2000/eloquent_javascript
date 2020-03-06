c98619d7a7940b766b861b52ce0407bbed8c819b
Darren Zhang <darrenzhang200027@gmail.com>
2020-03-06 00:45:09 -0500

    added range function

--- randomProblems.js
+++ randomProblems.js
@@ -1,17 +1,30 @@
 /*
 Check if a number can be reached by either multiplying by 3 or adding 5, starting with 1
 */
-function solution(num){
-    function find(cur, history){
-        if(cur == num){
+function solution(num) {
+    function find(cur, history) {
+        if (cur == num) {
             return history
-        }
-        else if(cur > num)
-            return null
-        else{
-            return find(cur * 3, `( ${history} * 3)`) || find(cur + 5, `( ${history} + 5 )`)
+        } else if (cur > num) return null
+        else {
+            return (
+                find(cur * 3, `( ${history} * 3)`) ||
+                find(cur + 5, `( ${history} + 5 )`)
+            )
         }
     }
     return find(1, "1")
 }
-console.log(solution(13))
+// console.log(solution(13))
+
+let range = (start, end, step) => {
+    if(step === undefined)
+        step = 1
+    let arr = []    
+    for(let i = start; i < end; i += step){
+        arr.push(i)
+    }
+    return arr;
+}
+
+console.log(range(1, 10, 2))
